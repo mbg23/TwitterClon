5409d6f367aaa37d2578e84b6ad1b81d0a2ea8da
Martin Bagnasco <martinbg23@gmail.com>
2014-07-31 22:24:00 -0300

    Inicializando repositorio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwitterClon::Application.config.secret_key_base = '605f9e1a14fd1435b2813a3950d8de9c34bffe1200975697a6326531177b83a86da2e97d8e2ef6778c1d25c8cfcd914d6ff8e1820470d8af91fdddbfd15d5ffb'
